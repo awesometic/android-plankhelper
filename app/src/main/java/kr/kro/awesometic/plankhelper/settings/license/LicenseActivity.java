1895b321780b068784da955d0d47dd766d868ef2
Awesometic <secugyu@gmail.com>
2017-06-03 23:48:17 +0900

    Add licenses for open source libraries in use

--- app/src/main/java/kr/kro/awesometic/plankhelper/settings/license/LicenseActivity.java
+++ app/src/main/java/kr/kro/awesometic/plankhelper/settings/license/LicenseActivity.java
@@ -1,8 +1,18 @@
 package kr.kro.awesometic.plankhelper.settings.license;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.support.v7.widget.ScrollingTabContainerView;
+import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import kr.kro.awesometic.plankhelper.R;
 
 /**
  * Created by Awesometic on 2017-06-03.
@@ -10,12 +20,41 @@ import android.widget.Toast;
 
 public class LicenseActivity extends AppCompatActivity {
 
+    @BindView(R.id.textView_license)
+    TextView tvLicense;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_license_act);
+        ButterKnife.bind(this);
 
-        Toast.makeText(this, "Yeah", Toast.LENGTH_SHORT).show();
+        // toolbar 설정
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        finish();
+        tvLicense.append(getResources().getString(R.string.settings_app_opensource_license_butterknife));
+        tvLicense.append(getResources().getString(R.string.settings_app_opensource_license_materialviewpager));
+        tvLicense.append(getResources().getString(R.string.settings_app_opensource_license_materialcalendarview));
+        tvLicense.append(getResources().getString(R.string.settings_app_opensource_license_advancedrecyclerview));
+        tvLicense.append(getResources().getString(R.string.settings_app_opensource_license_hellochart));
+
+        tvLicense.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
